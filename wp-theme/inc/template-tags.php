d661550e7f5604b02bcfc92e81073b909eb282e7
dixitprakhar25 <dixit.prakhar25@gmail.com>
2025-12-25 12:41:46 +0530

    Create template-tags.php for theme helper functions

---
+++ wp-theme/inc/template-tags.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Template Tags
+ * 
+ * Custom template tags for this theme
+ * 
+ * @package Rishikesh_Vedic
+ * @since 1.0.0
+ */
+
+if ( ! function_exists( 'rishikesh_vedic_posted_on' ) ) :
+    /**
+     * Prints HTML with meta information for the current post-date/time.
+     */
+    function rishikesh_vedic_posted_on() {
+        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+        
+        $time_string = sprintf(
+            $time_string,
+            esc_attr( get_the_date( DATE_W3C ) ),
+            esc_html( get_the_date() )
+        );
+
+        $posted_on = sprintf(
+            /* translators: %s: post date. */
+            esc_html_x( 'Posted on %s', 'post date', 'rishikesh-vedic' ),
+            '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+        );
+
+        echo '<span class="posted-on">' . $posted_on . '</span>';
+    }
+endif;
+
+if ( ! function_exists( 'rishikesh_vedic_posted_by' ) ) :
+    /**
+     * Prints HTML with meta information for the current author.
+     */
+    function rishikesh_vedic_posted_by() {
+        $byline = sprintf(
+            /* translators: %s: post author. */
+            esc_html_x( 'by %s', 'post author', 'rishikesh-vedic' ),
+            '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+        );
+
+        echo '<span class="byline"> ' . $byline . '</span>';
+    }
+endif;
+
+if ( ! function_exists( 'rishikesh_vedic_entry_footer' ) ) :
+    /**
+     * Prints HTML with meta information for the categories, tags and comments.
+     */
+    function rishikesh_vedic_entry_footer() {
+        // Hide category and tag text for pages.
+        if ( 'post' === get_post_type() ) {
+            /* translators: used between list items, there is a space after the comma */
+            $categories_list = get_the_category_list( esc_html__( ', ', 'rishikesh-vedic' ) );
+            if ( $categories_list ) {
+                /* translators: 1: list of categories. */
+                printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'rishikesh-vedic' ) . '</span>', $categories_list );
+            }
+
+            /* translators: used between list items, there is a space after the comma */
+            $tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'rishikesh-vedic' ) );
+            if ( $tags_list ) {
+                /* translators: 1: list of tags. */
+                printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'rishikesh-vedic' ) . '</span>', $tags_list );
+            }
+        }
+
+        if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+            echo '<span class="comments-link">';
+            comments_popup_link(
+                sprintf(
+                    wp_kses(
+                        /* translators: %s: post title */
+                        __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'rishikesh-vedic' ),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    wp_kses_post( get_the_title() )
+                )
+            );
+            echo '</span>';
+        }
+    }
+endif;
+
+if ( ! function_exists( 'rishikesh_vedic_post_thumbnail' ) ) :
+    /**
+     * Displays an optional post thumbnail.
+     */
+    function rishikesh_vedic_post_thumbnail() {
+        if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+            return;
+        }
+
+        if ( is_singular() ) :
+            ?>
+            <div class="post-thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+        <?php else : ?>
+            <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+                <?php
+                the_post_thumbnail(
+                    'post-thumbnail',
+                    array(
+                        'alt' => the_title_attribute(
+                            array(
+                                'echo' => false,
+                            )
+                        ),
+                    )
+                );
+                ?>
+            </a>
+        <?php
+        endif;
+    }
+endif;
+?>
